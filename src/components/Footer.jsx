a97c381d5a9a26d1af3428d6ec71dc5b117f60c3
Ilian Cholpanov <123170166+IlianCholpanov@users.noreply.github.com>
2024-05-19 13:44:17 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.copyright}>
-        &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        {new Date().getFullYear()}
       </p>
     </footer>
   );
