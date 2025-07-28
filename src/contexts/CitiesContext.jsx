6d0f9976e9d496c66eeca7ae834c89dc580673f5
Илиан Чолпанов <iliancholpanov@gmail.com>
2025-07-29 01:31:17 +0300

    Add api to CitiesContext.json

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://iliancholpanov.github.io/host_api/cities.json";
 
 const CitiesContext = createContext();
 
